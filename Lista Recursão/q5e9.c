59dfe94459ea8025effe82214f2aaa168e3245f9
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2021-06-07 16:46:49 -0300

    Update q5e9.c

--- Lista Recursão/q5e9.c
+++ Lista Recursão/q5e9.c
@@ -1,20 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
-int soma=0;
-/*Crie uma função recursiva que receba um número inteiro positivo N e calcule o
-somatório dos números de 1 a N. */
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <locale.h>
+    int soma=0;
+    /*Crie uma função recursiva que receba um número inteiro positivo N e calcule o
+    somatório dos números de 1 a N. */
 
-int somatorio(int n){
-    if(n==1){
-        return soma+=n;
+    int somatorio(int n){
+        if(n==1){
+            return soma+=n;
+        }
+        soma+=n;
+        somatorio(n-1);
     }
-    soma+=n;
-    somatorio(n-1);
-}
 
-int main(){
-    int n=0;
-    wprintf(L"Digite um número: ");
-    scanf("%d", &n);
-    printf("%d\n", somatorio(n));
-}
+    int main(){
+        int n=0;
+        setlocale(LC_ALL,"Portuguese");
+        wprintf(L"Digite um número: ");
+        scanf("%d", &n);
+        printf("%d\n", somatorio(n));
+    }
