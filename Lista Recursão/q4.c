064833f7e8b30616c5dde49a9e5fc262f170c35e
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2021-06-08 21:50:09 -0300

    Update q4.c

--- Lista Recursão/q4.c
+++ Lista Recursão/q4.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define TAM 5
-int cont=0;
-int soma=0;
+int cont=0, soma=0;
 // Faça uma função recursiva que permita somar os elementos de um vetor de inteiros
 int somaElementos (int *p)
 {
+    static int cont=0, soma=0;
     if(cont==TAM){
-        return soma;
+        int retorno = soma;
+        cont=0, soma=0;
+        return retorno;
     }
     soma+=p[cont];
     cont++;
@@ -17,12 +19,21 @@ int somaElementos (int *p)
 int main()
 {
     int *vet= malloc(TAM*sizeof(int));
+    int continuar=1;
+    
+    do{
     for(int i = 0; i< TAM; i++)
     {
+        printf("Digite o elemento %d: ", i+1);
+        printf("\n");
         scanf("%d", &vet[i]);
     }
     int result = 0;
     result = somaElementos(vet);
     printf("%d", result);
     free(vet);
+    printf("\nProsseguir/Iniciar/Continuar (1) Sair (0)\n");
+    scanf("%d", &continuar);
+    }
+    while(continuar);
 }
